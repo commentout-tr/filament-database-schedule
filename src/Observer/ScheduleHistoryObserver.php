3578a45b5c7a6f9062805f846b210bdab633b5f0
Onur Şahindur <onursahindur@gmail.com>
2024-02-22 12:00:11 +0300

    fix

--- src/Observer/ScheduleHistoryObserver.php
+++ src/Observer/ScheduleHistoryObserver.php
@@ -2,6 +2,7 @@
 
 namespace HusamTariq\FilamentDatabaseSchedule\Observer;
 
+use HusamTariq\FilamentDatabaseSchedule\Models\Schedule;
 use HusamTariq\FilamentDatabaseSchedule\Models\ScheduleHistory;
 
 class ScheduleHistoryObserver
@@ -13,11 +14,14 @@ class ScheduleHistoryObserver
     {
         $maximum_history_count = config('filament-database-schedule.history_max_count');
         if (isset($maximum_history_count)) {
-            $schedule = $scheduleHistory->command;
+            $schedule = Schedule::find($scheduleHistory->schedule_id);
             $history_count = $schedule->histories()->count();
             if ($history_count > $maximum_history_count) {
-                $keepIds = $schedule->histories()->latest()->take($maximum_history_count)->pluck('id');
-                $schedule->histories()->whereNotIn('id', $keepIds)->delete();
+                $willDeletedIds = $schedule->histories()
+                         ->orderBy('created_at','desc')
+                         ->skip($maximum_history_count)
+                         ->pluck('id');
+                ScheduleHistory::whereIn('id', $willDeletedIds)->delete();
             }
         }
     }
